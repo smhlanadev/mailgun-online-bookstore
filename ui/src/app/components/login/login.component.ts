388bb4012f5bca97bb1c0273ffc43dc5df7f5c36
Sesethu Mhlana <smhlana98@gmail.com>
2025-03-16 16:45:53 +0200

    Add login page form and styling.

--- ui/src/app/components/login/login.component.ts
+++ ui/src/app/components/login/login.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onLogin() {
+    if (this.loginForm?.valid) {
+      const { email, password } = this.loginForm.value;
+      
+      if (email === 'user@g.com' && password === 'password') {
+        this.router.navigate(['/main']);
+      } else {
+        alert('Invalid credentials');
+      }
+    }
+  }
 }
