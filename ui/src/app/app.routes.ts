28d54c7335408ad5f921708a61f22ed70d54756d
Sesethu Mhlana <smhlana98@gmail.com>
2025-03-16 16:17:14 +0200

    Added components (login and store) + added routing + added navbar.

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { StoreComponent } from './components/store/store.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'store', component: StoreComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/login' }
+];
