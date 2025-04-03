c79b618ef73dafe8295185536365ebee17d3af6c
Sesethu Mhlana <smhlana98@gmail.com>
2025-04-03 06:06:35 +0200

    Remove auth from routes.

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { AuthComponent } from './components/auth/auth.component';
 import { StoreComponent } from './components/store/store.component';
 
 export const routes: Routes = [
-    { path: 'auth', component: AuthComponent },
     { path: 'store', component: StoreComponent },
-    { path: '', redirectTo: '/auth', pathMatch: 'full' },
-    { path: '**', redirectTo: '/auth' }
+    { path: '', redirectTo: '/store', pathMatch: 'full' },
+    { path: '**', redirectTo: '/store' }
 ];
