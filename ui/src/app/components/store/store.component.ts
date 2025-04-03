57b1ab83ed3fbada7493ea54ddffe06163a926e7
Sesethu Mhlana <smhlana98@gmail.com>
2025-04-03 06:46:06 +0200

    Add subscription modal for newsletter sign-up.

--- ui/src/app/components/store/store.component.ts
+++ ui/src/app/components/store/store.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { BookService } from '../../services/book.service';
 import { CommonModule } from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { Book } from '../../models/book.model';
 import { MatDialog } from '@angular/material/dialog';
 import { PurchaseModalComponent } from '../purchase-modal/purchase-modal.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { NotificationService } from '../../services/notification.service';
-import { EmailType } from '../../models/email-type.enum';
-import { UserService } from '../../services/user.service';
-import { NotificationInput } from '../../models/notification-input.model';
+import { SubscribeModalComponent } from '../subscribe-modal/subscribe-modal.component';
 
 @Component({
   selector: 'app-store',
@@ -25,8 +22,6 @@ export class StoreComponent {
     private bookService: BookService,
     private dialog: MatDialog,
     private snackBar: MatSnackBar,
-    private notificationService: NotificationService,
-    private userService: UserService
   ) {
     this.books = this.bookService.getBooks();
   }
@@ -48,21 +43,15 @@ export class StoreComponent {
   onSubscribe(event?: Event) {
     event?.preventDefault();
 
-    const input: NotificationInput = { 
-      emailTo: this.userService.currentUser?.email || '', 
-      emailType: EmailType.Subscription, 
-      name: this.userService.currentUser?.name || '' 
-    };
-
-    this.notificationService.send(input).subscribe({
-      next: () => {
-        this.snackBar.open('Subscribed to newsletter!', 'Close', { duration: 3000 });
-      },
-      error: error => {
-        console.error('Error sending notification', error);
-        this.snackBar.open('Something went wrong. Try again later.', 'Close', { duration: 3000 });
-      }
+    const dialogRef = this.dialog.open(SubscribeModalComponent, {
+      width: '25rem',
+      minHeight: '21rem',
+      maxHeight: '24rem',
+      disableClose: true
     });
-    
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.snackBar.open(result ? 'Subscribed to newsletter!' : 'Something went wrong. Try again later.', 'Close', { duration: 3000 });
+    });
   }
 }
